c82cfb65412ce12e56f7684c051c502b3cea7e94
potochii <103028133+potochii@users.noreply.github.com>
2023-07-12 21:33:31 +0700

    Add files via upload

---
+++ app/Comment.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+
+    protected $fillable = [
+
+        'topic_id',
+         'comment',
+          'user_id',
+    ];  
+    public function user(){
+
+        return $this->hasOne(User::class,'id','user_id');
+    }
+}
---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Comment;
+use App\Http\Requests\CommentRequest;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $ments=comment::all();
+
+        return view('home', compact('ments'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CommentRequest $request)
+    {
+        $commentss = new Comment();
+
+        $request->request->add(['user_id' => auth()->user()->id]);
+        $commentss->create($request->only(['topic_id', 'comment', 'user_id']));
+
+        return back()->with('save_status', [
+            'message' => 'Your comment saved',
+            'status' => 'success'
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+   public function delete($id){
+      // $delete=Comment::onlyTrashed()->find($id)->forceDelete();
+       //
+       $des=comment::find($id);
+       $des->delete();
+       return back()->with('save_status', [
+        'message' => 'Your delete ',
+        'status' => 'success'
+    ]);
+   }
+
+}
